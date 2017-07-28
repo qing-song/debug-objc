fd9cb7d6b48f23c838eccec8300ceb26b4b55ff8
qingsong <qs_hero@163.com>
2017-07-28 17:14:16 +0800

    add Project

---
+++ objc4-706/debug-objc/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by qingsong on 2017/7/28.
+//
+//
+
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
